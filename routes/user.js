208f866abf6c02e2af8abb59e0f3928a672bb8ee
Dinuka Yasintha <141404139+dinukayasi22@users.noreply.github.com>
2025-11-07 22:08:37 +0530

    updated the route

--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,7 @@ import { loginUser, getUserProfile, updateUserProfile, registerAdmin } from '../
 import { protect } from '../middleware/auth.js';
 import { check } from 'express-validator';
 
-router.post('/users/register/admin', [
+router.post('/users/admin/register', [
     check('name', 'Name is required').not().isEmpty(),
     check('email', 'Please include a valid email').isEmail(),
     check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 }),
