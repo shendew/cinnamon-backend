9196edc4209d5ed3cca4b8bd2fb201f9cacd728d
Dinuka Yasintha <141404139+dinukayasi22@users.noreply.github.com>
2025-11-15 02:18:34 +0530

    added farmer operations
    Let farmer create farm records and cultivation records.
    Also updated database schema.
    Updated routes of farmer

--- src/db/schema.js
+++ src/db/schema.js
@@ -1,5 +1,5 @@
 import { timestamptz } from 'drizzle-orm/gel-core';
-import { pgTable, serial, text, date } from 'drizzle-orm/pg-core';
+import { pgTable, serial, integer, numeric, text, date } from 'drizzle-orm/pg-core';
 
 export const role = pgTable('role', {
   role_id: serial('role_id').primaryKey(),
@@ -10,7 +10,7 @@ export const role = pgTable('role', {
 export const user = pgTable('user', {
   user_id: serial('user_id').primaryKey(),
   name: text('name').notNull(),
-  email: text('email').notNull(),
+  email: text('email').notNull().unique(),
   password_hash: text('password_hash').notNull(),
   phone: text('phone').notNull(),
   role_id: serial('role_id').references(() => role.role_id),
@@ -23,7 +23,7 @@ export const user = pgTable('user', {
 export const farmer_profile = pgTable('farmer_profile', {
   farmer_id: serial('farmer_id').primaryKey(),
   user_id: serial('user_id').references(() => user.user_id, { onDelete: 'cascade' }).notNull(),
-  nic: text('nic').notNull(),
+  nic: text('nic').notNull().unique(),
   address: text('address').notNull(),
   gender: text('gender').notNull(),
   date_of_birth: date('date_of_birth'),
@@ -31,3 +31,28 @@ export const farmer_profile = pgTable('farmer_profile', {
   updated_at: timestamptz('updated_at').defaultNow()
 });
 
+
+export const farms = pgTable('farms', {
+  farm_id: serial('farm_id').primaryKey(),
+  farmer_id: serial('farmer_id').references(() => farmer_profile.farmer_id, { onDelete: 'cascade' }).notNull(),
+  farm_name: text('farm_name').notNull(),
+  gps_coordinates: text('gps_coordinates').notNull(),
+  area_acres: numeric('area_acres').notNull(),
+  created_at: timestamptz('created_at').defaultNow(),
+  updated_at: timestamptz('updated_at').defaultNow()
+});
+
+export const cultivation = pgTable('cultivation', {
+  batch_id: serial('batch_id').primaryKey(),
+  farm_id: serial('farm_id').references(() => farms.farm_id, { onDelete: 'cascade' }).notNull(),
+  farmer_id: serial('farmer_id').references(() => farmer_profile.farmer_id, { onDelete: 'cascade' }).notNull(),
+  date_of_planting: date('date_of_planting').notNull(),
+  seeding_source: text('seeding_source').notNull(),
+  type_of_fertilizer: text('type_of_fertilizer').notNull(),
+  pesticides: text('pesticides').notNull(),
+  organic_certification: text('organic_certification').notNull(),
+  expected_harvest_date: date('expected_harvest_date').notNull(),
+  no_of_trees: integer('no_of_trees').notNull(),
+  created_at: timestamptz('created_at').defaultNow(),
+  updated_at: timestamptz('updated_at').defaultNow()
+});
