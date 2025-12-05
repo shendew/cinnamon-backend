054cd53a918b035f82f47271e179809a7a4757e0
Dinuka Yasintha <141404139+dinukayasi22@users.noreply.github.com>
2025-12-05 20:17:33 +0530

    added cultivation, and harvesting operations
    Additionally added collector authentications.
    Bla bla bla

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ import express from "express";
 import "dotenv/config";
 import userRoutes from "./routes/user.js";
 import farmerRoutes from "./routes/farmer.js";
+import collectorRoutes from "./routes/collector.js";
 
 const app = express();
 app.use(express.json());
 
 app.use("/api", userRoutes);
 app.use("/api/farmer", farmerRoutes);
+app.use("/api/collector", collectorRoutes);
 
 const port = process.env.PORT || 3000;
 
